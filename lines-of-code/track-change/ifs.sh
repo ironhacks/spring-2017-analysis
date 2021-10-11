1887c72d8b4408c59a837bb6ff12bd9653777010
jialincheoh <jcheoh@purdue.edu>
2021-10-11 16:39:55 +0800

    included script for loc extraction

---
+++ lines-of-code/track-change/ifs.sh
@@ -0,0 +1,5 @@
+file=abc.csv
+while IFS= read -r line
+do
+    diff spring2017-unal-goldironhack-phase2/2017-Purdue-UNAL-IronHack-$line spring2017-unal-goldironhack-phase1/2017-Purdue-UNAL-IronHack-$line  | diffstat
+done < "$file"
