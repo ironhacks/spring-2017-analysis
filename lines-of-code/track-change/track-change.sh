5d28a8013bb8c6b3a0058e4309fd3ff120b07f62
jialincheoh <jcheoh@purdue.edu>
2021-10-11 16:42:27 +0800

    track change

--- lines-of-code/track-change/track-change.sh
+++ lines-of-code/track-change/track-change.sh
@@ -1,4 +1,4 @@
-file=abc.csv
+file=name-list.csv
 while IFS= read -r line
 do
     diff spring2017-unal-goldironhack-phase2/2017-Purdue-UNAL-IronHack-$line spring2017-unal-goldironhack-phase1/2017-Purdue-UNAL-IronHack-$line  | diffstat
