27830684497afdd076f4bf28da1cbb4d673b5b41
jialincheoh <jcheoh@purdue.edu>
2022-03-01 23:18:09 +0800

    structure of git repo

---
+++ spring-2017/script/analysis/EFA.R
@@ -0,0 +1,45 @@
+# remove "pass class" survey item 13
+motivation <- read.table("/Users/jialincheoh/Desktop/survey-motivation-WQ13.csv", header=TRUE, sep = ",")
+
+# Loading required package: psych
+require(psych)
+
+# Loading required package: GPArotation
+require(GPArotation)
+
+describe(motivation)
+
+lowerCor(motivation)
+
+library(correlation)
+
+correlation::correlation(motivation,
+                         include_factors = TRUE, method = "auto"
+)
+
+
+fa.parallel(motivation, fm='ml', fa='fa', main="Parallel Analysis Scree Plots", n.iter=100, error.bars=T, sim=F)
+
+fa(r=motivation, nfactors=2, rotate='promax', fm='ml')
+
+# looks like oblimin rotation is the best case for us right now. 
+
+fa(r=motivation, nfactors=2, rotate='oblimin', fm='ml')
+
+fa(r=motivation, nfactors=2, rotate='varimax', fm='ml')
+
+# remove "pass class" survey item 13 and "money" survey item 1
+motivation <- read.table("/Users/jialincheoh/Desktop/survey-motivation-WQ13-WQ1.csv", header=TRUE, sep = ",")
+
+lowerCor(motivation)
+
+fa.parallel(motivation, fm='ml', fa='fa', main="Parallel Analysis Scree Plots", n.iter=100, error.bars=T, sim=F)
+
+fa(r=motivation, nfactors=2, rotate='promax', fm='ml')
+
+fa(r=motivation, nfactors=2, rotate='oblimin', fm='ml')
+
+fa(r=motivation, nfactors=2, rotate='varimax', fm='ml')
+
+
+
